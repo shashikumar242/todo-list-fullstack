e3fa2af54a0d54d2193e65d98fdf9c52e58353e8
ShashiKumar242 <shashikumar.g.242@gmail.com>
2024-01-09 21:41:34 +0530

    project-completed

---
+++ backend/Server.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const routes = require("./routes/ToDoRoute");
+
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverSelectionTimeoutMS: 5000, // Adjust timeout settings if required
+    // ... other options
+  })
+  .then(() => console.log(`Connected to MongoDB...`))
+  .catch((err) => console.log(`Failed to connect to MongoDB`, err));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log(`Listening on : ${PORT}`));
